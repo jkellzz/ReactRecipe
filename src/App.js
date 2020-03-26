4f97587135579a3731d3c112d06e4026a3f97f83
Johnathan Kelley <jkellz592@gmail.com>
2020-03-26 16:27:17 -0400

    Added simple search button. (not functional)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from './components/Form'
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form/>
       </div>
     );
   }
---
+++ src/components/Form.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Form = () => (
+    <form>
+        <input type='text' />
+        <button>Search</button>
+    </form>
+)
+
+export default Form
