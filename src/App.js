466ac27bf6df5f5041bc17ad345152c67c120190
Johnathan Kelley <jkellz592@gmail.com>
2020-03-26 22:57:57 -0400

    Commented out the api call to limit requests made when saving

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ const App = () => {
   const APP_KEY = 'e4fdce8ff93a647ccbf1cd56de0ed3d1'
 
 
-  useEffect( () => {
-    getRecipes()
-  }, [])
+  // useEffect( () => {
+  //   getRecipes()
+  // }, [])
 
-  const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
-    const data = await response.json()
-    console.log(data)
-  }
+  // const getRecipes = async () => {
+  //   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+  //   const data = await response.json()
+  //   console.log(data)
+  // }
 
 
   return(
     <div className='App'>
-     <h1>Hello React</h1>
-     <form className='sarch-form'>
+     <h1>I want to make...</h1>
+     <form className='search-form'>
        <input className='search-bar' type='text'/>
        <button className='search-button' type='submit'>Search</button>
      </form>
