8887848277d397f7b7604b3821a47fce5c15b1be
Johnathan Kelley <jkellz592@gmail.com>
2020-03-29 00:13:00 -0400

    After removing the header component I was able to get things rendering with specific data on the page.

--- src/App.js
+++ src/App.js
@@ -9,37 +9,50 @@ const App = () => {
   const APP_KEY = 'e4fdce8ff93a647ccbf1cd56de0ed3d1'
 
   const [recipes, setRecipes] = useState([])
-  
-
+  const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
 
 
   useEffect( () => {
     getRecipes()
-  }, [])
+  }, [query])
 
   const getRecipes = async () => {
-    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`)
     const data = await response.json()
     setRecipes(data.hits)
     console.log(data.hits)
   }
 
+const updateSearch = e => {
+  setSearch(e.target.value)
+}
 
+const getSearch = e => {
+  e.preventDefault()
+  setQuery(search)
+  setSearch('')
+}
 
   return(
     <div className='App'>
      <h1>I want to make...</h1>
-     <form className='search-form'>
-     <input className='search-bar' type='text'/>
+     <form onSubmit={getSearch} className='search-form'>
+     <input className='search-bar' type='text' value={search}
+     onChange={updateSearch}/>
      <button className='search-button' type='submit'>Search</button>
      </form>
+     <div className='Rec'>
     {recipes.map(recipe => (
           <Recipe 
+          key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
           image={recipe.recipe.image}
+          ingredients={recipe.recipe.ingredients}
           />
     ))}
+    </div>
      </div>
   )
 }
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const Recipe = ({title,calories,image}) => {
+const Recipe = ({title,calories,image,ingredients}) => {
     return( 
         <div>
             <h1>{title}</h1>
-            <p>{calories}</p>
             <img src={image} alt=''/>
+            <p>Calories {calories}</p>      
+                {ingredients.map(ingredient => (
+                    <li>{ingredient.text}</li>
+                )
+                    )}
+            
         </div>
     )
 }
