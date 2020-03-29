815fdad2797bf22048fcf08af2d18d267b1fcc75
Johnathan Kelley <jkellz592@gmail.com>
2020-03-28 23:51:34 -0400

    Major issues with header component. Needed to scrap it and debug till issue is found.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import Recipe from './components/Recipe'
 import './App.css';
-import Header from './components/Header'
+// import Header from './components/Header'
 
 
 const App = () => {
@@ -9,29 +9,36 @@ const App = () => {
   const APP_KEY = 'e4fdce8ff93a647ccbf1cd56de0ed3d1'
 
   const [recipes, setRecipes] = useState([])
+  
 
 
-  // useEffect( () => {
-  //   getRecipes()
-  // }, [])
 
-  // const getRecipes = async () => {
-  //   const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
-  //   const data = await response.json()
-  //   setRecipes(data.hits)
-  //   console.log(data.hits)
-  // }
+  useEffect( () => {
+    getRecipes()
+  }, [])
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    setRecipes(data.hits)
+    console.log(data.hits)
+  }
+
 
 
   return(
-    <div>
-    <Header/>
+    <div className='App'>
+     <h1>I want to make...</h1>
+     <form className='search-form'>
+     <input className='search-bar' type='text'/>
+     <button className='search-button' type='submit'>Search</button>
+     </form>
     {recipes.map(recipe => (
           <Recipe 
-          key={recipe.recipe.label}
           title={recipe.recipe.label}
           calories={recipe.recipe.calories}
-          image={recipe.recipe.image}/>
+          image={recipe.recipe.image}
+          />
     ))}
      </div>
   )
